1e9c1895fe5f90977346d98f68a028ca36fe7229
ZandreeTresvalles <zandree.tresvalles@gmail.com>
2020-12-15 19:35:56 +0800

    main

--- src/main/java/com/accenture/bars/entity/Billing.java
+++ src/main/java/com/accenture/bars/entity/Billing.java
@@ -3,6 +3,7 @@ package com.accenture.bars.entity;
 import java.time.LocalDate;
 import java.util.Set;
 import com.accenture.bars.entity.Account;
+
 public class Billing {
 	private int billingId;
 	private int billingCycle;
@@ -12,5 +13,53 @@ public class Billing {
 	private LocalDate endDate;
 	private String lastEdited;
 	private Account account;
+	public int getBillingId() {
+		return billingId;
+	}
+	public void setBillingId(int billingId) {
+		this.billingId = billingId;
+	}
+	public int getBillingCycle() {
+		return billingCycle;
+	}
+	public void setBillingCycle(int billingCycle) {
+		this.billingCycle = billingCycle;
+	}
+	public String getBillingMonth() {
+		return billingMonth;
+	}
+	public void setBillingMonth(String billingMonth) {
+		this.billingMonth = billingMonth;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+	public String getLastEdited() {
+		return lastEdited;
+	}
+	public void setLastEdited(String lastEdited) {
+		this.lastEdited = lastEdited;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 
 }
